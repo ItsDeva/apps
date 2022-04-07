8a18f564745b55899714a7ae525df9fdd333ad4b
Jenkins Admin <ndevaraj424@gmail.com>
2022-04-07 05:56:01 +0000

    Latest Web from Jenkins

--- ams/flutter_service_worker.js
+++ ams/flutter_service_worker.js
@@ -26,8 +26,8 @@ const RESOURCES = {
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
 "index%20copy.html": "16fbc507bf6b1b9a4998919c41972e9c",
 "index-old.html": "a8ded0a6b8c439c14360a2251df84189",
-"index.html": "f4ae59453f1f1a7982ca2253b4631716",
-"/": "f4ae59453f1f1a7982ca2253b4631716",
+"index.html": "3610f10b4b930cc35220c36991b38694",
+"/": "3610f10b4b930cc35220c36991b38694",
 "manifest.json": "2a465facf2597639b4febc1170e6c07a"
 };
 
