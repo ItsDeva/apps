404379ab56866e63d0881903272e751320d78afc
Jenkins Admin <ndevaraj424@gmail.com>
2022-05-16 21:22:32 +0000

    Jenkins Admin Build - BUILD ID: 50

--- ams/flutter_service_worker.js
+++ ams/flutter_service_worker.js
@@ -8,7 +8,7 @@ const RESOURCES = {
 "canvaskit/profiling/canvaskit.js": "ae2949af4efc61d28a4a80fffa1db900",
 "canvaskit/profiling/canvaskit.wasm": "95e736ab31147d1b2c7b25f11d4c32cd",
 "flutter.js": "3688efe0a39e59781b4f95efbd6b5b62",
-"main.dart.js": "5ea5168d648bbc9939a1e1b4ca7f9193",
+"main.dart.js": "4e780910c9d4d832f9e30064740d5d4e",
 "version.json": "a3d138c5abc0063c9c5f6b2330eb29cf",
 "assets/assets/fonts/Aliey-BoldItalic.otf": "f2f763b93b892fe51660c0600b141cf6",
 "assets/assets/fonts/Aliey-Bold.otf": "ec3d3b9ece5e94b4004883dded897cf6",
@@ -37,8 +37,8 @@ const RESOURCES = {
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
 "index%20copy.html": "16fbc507bf6b1b9a4998919c41972e9c",
 "index-old.html": "a8ded0a6b8c439c14360a2251df84189",
-"index.html": "b0a4c1687495423dbf5db502a035d7dd",
-"/": "b0a4c1687495423dbf5db502a035d7dd",
+"index.html": "37815652c657bfc567d85679f316aef2",
+"/": "37815652c657bfc567d85679f316aef2",
 "manifest.json": "2a465facf2597639b4febc1170e6c07a",
 "jsQR.js": "d54b99cd32c7a3c35dc7df755ee1e137"
 };
